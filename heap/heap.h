b7c684444adb47c85cb927a19eee559e69dc307f
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-05-25 21:14:55 +0300

    Update heap.h

--- heap/heap.h
+++ heap/heap.h
@@ -23,7 +23,7 @@ public:
     /* Free a block allocated by `heap::malloc', `heap::realloc' or `heap::calloc'.  */
     static void free(void *__add);
 
-    /* Free all the memory blocks in the record. */
+    /* Free all the memory blocks. */
     static void tear_down(void);
 
 };
