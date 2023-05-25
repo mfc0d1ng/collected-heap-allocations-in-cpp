944e18bffd8bebaeb5c291fd4c274ca913b2fa0f
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-05-25 14:16:29 +0300

    Update main.cpp

--- heap/main.cpp
+++ heap/main.cpp
@@ -7,6 +7,8 @@ int main(int argc, char *argv[])
     char *ptr = (char *) heap::malloc(12);
     memcpy(ptr, "Hello world", 12);
 
+    std::puts(ptr);
+    
     ptr = (char *) heap::realloc(ptr, 20);
     memcpy(ptr, "Hello world, Again.", 20);
 
