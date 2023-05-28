50cfc7dc4ef5504f63566edea86f33ef3a2a65b8
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-05-28 18:52:18 +0300

    Update heap.cpp
    Updating entire heap.cpp

--- heap/heap.cpp
+++ heap/heap.cpp
@@ -5,13 +5,7 @@
 #define memerror  "Out of memory, program terminated."
 #define ptrerror  "Invalid pointer, program terminated."
 
-struct location
-{
-    void *heap_location;
-    unsigned char status;
-}__attribute__((packed));
-
-static std::vector<struct location> record;
+static std::vector<void*> record = {};
 
 static void print_memerror(void)
 {
@@ -31,8 +25,7 @@ void *heap::malloc(std::size_t __size)
     void *heap_ptr = std::malloc(__size);
     if(heap_ptr != nullptr)
     {
-        struct location location = {heap_ptr, __alloc};
-        record.push_back(location);
+        record.push_back(heap_ptr);
     }
     else
     {
@@ -49,16 +42,15 @@ void *heap::realloc(void *__prev, std::size_t __size)
         return heap::malloc(__size);
     }
     bool found = false;    
-    void *heap_ptr =  std::realloc(__prev, __size);
+    void *heap_ptr = std::realloc(__prev, __size);
     if(heap_ptr != nullptr)
     {
         for(std::size_t i = record.size(); i > 0; )
         {
-            --i;
-            if(record[i].heap_location == __prev)
+            if(record[--i] == __prev)
             {
                 found = true;
-                record[i] = {heap_ptr, __alloc};
+                record[i] = heap_ptr;
                 break;
             }
         }
@@ -80,8 +72,7 @@ void *heap::calloc(std::size_t __nmemb, std::size_t __size)
     void *heap_ptr = std::calloc(__nmemb, __size);
     if(heap_ptr != nullptr)
     {
-        struct location location = {heap_ptr, __alloc};
-        record.push_back(location); 
+        record.push_back(heap_ptr); 
     }
     else
     {
@@ -91,21 +82,20 @@ void *heap::calloc(std::size_t __nmemb, std::size_t __size)
 }
 
 
-void heap::free(void *__add)
+void heap::free(void *__ptr)
 {
-    if(__add == nullptr)
+    if(__ptr == nullptr)
     {
         return;
     }
     bool found = false;
     for(std::size_t i = record.size(); i > 0; )
     {
-        --i;
-        if((record[i].heap_location == __add) && record[i].status == __alloc)
+        if(record[--i] == __ptr)
         {
             found = true;
-            record[i].status = __freed;
-            std::free(record[i].heap_location);
+            std::free(record[i]);
+            record[i] = nullptr;
             break;
         }
     }
@@ -116,18 +106,65 @@ void heap::free(void *__add)
 }
 
 
-void heap::tear_down(void)
+void heap::show_record(void)
 {
-    if(record.empty())
+    if(record.size())
+    {
+        std::printf("\nYour heap allocations starts at\n%c", '{');
+    }
+    else
+    {
+        std::puts("\nNo memory blocks has been allocated.");
+    }
+    std::size_t i = 0;
+    for(std::size_t j = record.size(); i < j; i++)
+    {
+        if(record[i] != nullptr)
+        {
+            if(i && i % 4 == 0)
+            {
+                printf("%s", "\n ");
+            }
+            std::printf("%p, ", record[i]);
+        }
+    }
+    i ? std::printf("\033[2D%c\n", '}') : 0;
+}
+
+
+std::size_t heap::count(void)
+{
+    return record.size();
+}
+
+
+void heap::erase(std::size_t __nb)
+{
+    if(__nb > record.size())
     {
         return;
     }
-    for(std::size_t i = record.size(); i > 0; )
+    std::size_t index = record.size();
+    for(std::size_t i = 0; i < __nb; ++i)
     {
-        --i;
-        if(record[i].status == __alloc)
+        if(record[--index] != nullptr)
         {
-            std::free(record[i].heap_location);
+            std::free(record[index]); 
+        }
+    }
+    record.resize(index); 
+}
+
+
+void heap::tear_down(void)
+{
+    for(std::size_t index = record.size(); index > 0; )
+    {
+        if(record[--index] != nullptr)
+        {
+            std::free(record[index]); 
         }
     }
 }
+
+
