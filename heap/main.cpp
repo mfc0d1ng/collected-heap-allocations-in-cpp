08075b26c96525934c8e6e3a3230aa876dd79fbb
Mohamed fareed <131618380+mfc0d1ng@users.noreply.github.com>
2023-05-28 18:51:21 +0300

    Update main.cpp
    Updating entire main.cpp

--- heap/main.cpp
+++ heap/main.cpp
@@ -5,21 +5,35 @@
 
 int main(int argc, char *argv[])
 {
-    char *ptr = (char *) heap::malloc(12);
+    char *ptr = (char *)heap::malloc(12);
     memcpy(ptr, "Hello world", 12);
 
     std::puts(ptr);
-    
-    ptr = (char *) heap::realloc(ptr, 20);
+
+    ptr = (char *)heap::realloc(ptr, 20);
     memcpy(ptr, "Hello world, Again.", 20);
 
-    std::puts(ptr); 
+    std::puts(ptr);
+
+    ptr = (char *)heap::realloc(ptr, 33);
+    memcpy(ptr, "Hello world, Again and Welcome.", 33);
+
+    std::puts(ptr);
     
-    /*  
-    // Try this
-    std::free(ptr);
-    */
-    
+    for (size_t i = 0; i < 50; i++)
+    {
+        heap::malloc(25); 
+    }
+
+    heap::show_record(); 
+
+    std::cout << "\nYou have allocated " << heap::count() << " memory blocks\n";
+
+    // Try this */
+    /* std::free(ptr); */
+    /* std::free(heap::malloc(25)); */
+
     heap::tear_down();
+
     return EXIT_SUCCESS;
 }
